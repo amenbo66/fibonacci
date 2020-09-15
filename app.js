b6ed841b289791c83f1ba2166e757075429ac0c1
risa <risris2546@gmail.com>
2020-09-15 16:42:10 +0900

    コメントを追加

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 'use strict';
 const memo = new Map();
+//n=0とn=1は決まっているものなので最初から入れておく
 memo.set(0,0);
 memo.set(1,1);
 function fib(n){
-    if (memo.has(n)){
+    if (memo.has(n)){ //memoにデータがあればそれを使う
         return memo.get(n);
     }
+    //memoにデータがないので計算する
     const value = fib(n-1) + fib(n-2);
     memo.set(n, value);
     return value;
